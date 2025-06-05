39156c4b91fb80ec1ab5ed64ea63091c395462ea
Pikulkaew Boonpeng <51871643+PKboon@users.noreply.github.com>
2025-06-05 12:38:24 -0400

    fetch members from db

--- src/app/members/page.tsx
+++ src/app/members/page.tsx
@@ -1,5 +1,16 @@
-const MembersPage = () => {
-	return <div>MembersPage</div>;
+import { getMembers } from "../actions/memberActions";
+
+const MembersPage = async () => {
+	const members = await getMembers();
+
+	return (
+		<div>
+			<ul>
+				{members &&
+					members.map((member) => <li key={member.id}>{member.name}</li>)}
+			</ul>
+		</div>
+	);
 };
 
 export default MembersPage;
