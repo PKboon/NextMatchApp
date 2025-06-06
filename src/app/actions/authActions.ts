4eae0ee6f1f08562aebc3b91a43bd6edded6978e
Pikulkaew Boonpeng <51871643+PKboon@users.noreply.github.com>
2025-06-05 23:07:39 -0400

    Section 5: Like feature (#3)
    * create like db table
    
    * add like button and its actions
    
    * implement list page content
    
    * fix tab content flashing when changing tab

--- src/app/actions/authActions.ts
+++ src/app/actions/authActions.ts
@@ -3,7 +3,7 @@
 import bcrypt from "bcryptjs";
 import { AuthError } from "next-auth";
 
-import { signIn, signOut } from "@/auth";
+import { auth, signIn, signOut } from "@/auth";
 import { User } from "@/generated/prisma";
 import { prisma } from "@/lib/prisma";
 import { LoginSchema } from "@/lib/schemas/loginSchema";
@@ -84,4 +84,13 @@ export async function getUserById(id: string) {
 	return prisma.user.findUnique({
 		where: { id },
 	});
+}
+
+export async function getAuthUserId() {
+	const session = await auth();
+	const userId = session?.user?.id;
+
+	if (!userId) throw new Error("Unauthorized");
+
+	return userId;
 }
--- src/app/members/MemberCard.tsx
+++ src/app/members/MemberCard.tsx
@@ -1,15 +1,26 @@
+"use client";
+
 import { Card, CardFooter } from "@heroui/card";
 import { Image } from "@heroui/image";
 import Link from "next/link";
 
+import LikeButton from "@/components/ui/LikeButton";
 import { Member } from "@/generated/prisma";
 import { calculateAge } from "@/lib/util";
 
 type Props = {
 	member: Member;
+	likeIds: string[];
 };
 
-const MemberCard = ({ member }: Props) => {
+const MemberCard = ({ member, likeIds }: Props) => {
+	const hasLiked = likeIds.includes(member.userId);
+
+	const preventLinkAction = (e: React.MouseEvent) => {
+		e.preventDefault();
+		e.stopPropagation();
+	};
+
 	return (
 		<Card fullWidth as={Link} href={`/members/${member.userId}`} isPressable>
 			<Image
@@ -19,6 +30,13 @@ const MemberCard = ({ member }: Props) => {
 				src={member.image || "/image/user.png"}
 				className="aspect-square object-cover"
 			/>
+
+			<div onClick={preventLinkAction}>
+				<div className="absolute top-3 right-3 z-50">
+					<LikeButton targetId={member.userId} hasLiked={hasLiked} />
+				</div>
+			</div>
+
 			<CardFooter className="absolute flex justify-start bg-dark-gradient overflow-hidden bottom-0 z-10">
 				<div className="flex flex-col text-white">
 					<span className="font-semibold">
