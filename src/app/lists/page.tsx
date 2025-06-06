a16e5d5a55547799b93ae1989c6dd46a91ce67a2
Pikulkaew Boonpeng <51871643+PKboon@users.noreply.github.com>
2025-06-05 22:41:15 -0400

    implement list page content

--- src/app/lists/page.tsx
+++ src/app/lists/page.tsx
@@ -1,5 +1,24 @@
-const ListsPage = () => {
-	return <div>ListsPage</div>;
+import {
+	fetchCurrentUserLikeIds,
+	fetchLikedMembers,
+} from "../actions/likeActions";
+import ListTab from "./ListTab";
+
+const ListsPage = async ({
+	searchParams,
+}: {
+	searchParams: Promise<{ type: string }>;
+}) => {
+	const { type } = await searchParams;
+
+	const likeIds = await fetchCurrentUserLikeIds();
+	const members = await fetchLikedMembers(type);
+
+	return (
+		<div>
+			<ListTab members={members} likeIds={likeIds} />
+		</div>
+	);
 };
 
 export default ListsPage;
--- src/app/members/page.tsx
+++ src/app/members/page.tsx
@@ -1,10 +1,10 @@
-import { fetchCurrentLikeIds } from "../actions/likeActions";
+import { fetchCurrentUserLikeIds } from "../actions/likeActions";
 import { getMembers } from "../actions/memberActions";
 import MemberCard from "./MemberCard";
 
 const MembersPage = async () => {
 	const members = await getMembers();
-	const likeIds = await fetchCurrentLikeIds();
+	const likeIds = await fetchCurrentUserLikeIds();
 
 	return (
 		<div className="mt-10 grid grid-cols-2 md:grid-cols-3 xl:grid-cols-6 gap-8">
