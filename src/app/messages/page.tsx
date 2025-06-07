4d6bfbb42637e7e4db0d04d458bb2b329225f3c2
Pikulkaew Boonpeng <51871643+PKboon@users.noreply.github.com>
2025-06-07 00:37:44 -0400

    add message's page a table

--- src/app/messages/page.tsx
+++ src/app/messages/page.tsx
@@ -1,12 +1,23 @@
+import { getMessagesByContainer } from "../actions/messageActions";
 import MessageSidebar from "./MessageSidebar";
+import MessageTable from "./MessageTable";
 
-const MessagesPage = () => {
+const MessagesPage = async ({
+	searchParams,
+}: {
+	searchParams: Promise<{ container: string }>;
+}) => {
+	const { container } = await searchParams;
+	const messages = await getMessagesByContainer(container);
+
 	return (
 		<div className="grid grid-cols-12 gap-5 h-[80vh] mt-10">
 			<div className="col-span-2">
 				<MessageSidebar />
 			</div>
-			<div className="col-span-10">yay</div>
+			<div className="col-span-10">
+				<MessageTable messages={messages} />
+			</div>
 		</div>
 	);
 };
