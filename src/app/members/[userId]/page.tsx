2db739956c8e211d028783d39b9bd785495bf79e
Pikulkaew Boonpeng <51871643+PKboon@users.noreply.github.com>
2025-06-06 20:55:36 -0400

    refactor

--- src/app/members/[userId]/page.tsx
+++ src/app/members/[userId]/page.tsx
@@ -1,8 +1,7 @@
-import { CardBody, CardHeader } from "@heroui/card";
-import { Divider } from "@heroui/divider";
 import { notFound } from "next/navigation";
 
 import { getMemberByUserId } from "@/app/actions/memberActions";
+import CardInnerWrapper from "@/components/CardInnerWrapper";
 
 const MemberDetailPage = async ({
 	params,
@@ -15,13 +14,7 @@ const MemberDetailPage = async ({
 	if (!member) return notFound();
 
 	return (
-		<>
-			<CardHeader className="text-2xl font-semibold text-secondary">
-				Profile
-			</CardHeader>
-			<Divider />
-			<CardBody></CardBody>
-		</>
+		<CardInnerWrapper header="Profile" body={<div>{member.description}</div>} />
 	);
 };
 export default MemberDetailPage;
--- src/app/members/[userId]/photos/page.tsx
+++ src/app/members/[userId]/photos/page.tsx
@@ -1,8 +1,7 @@
-import { CardBody, CardHeader } from "@heroui/card";
-import { Divider } from "@heroui/divider";
 import { Image } from "@heroui/image";
 
 import { getMemberPhotosByUserId } from "@/app/actions/memberActions";
+import CardInnerWrapper from "@/components/CardInnerWrapper";
 
 const MemberPhotosPage = async ({
 	params,
@@ -12,28 +11,24 @@ const MemberPhotosPage = async ({
 	const { userId } = await params;
 	const photos = await getMemberPhotosByUserId(userId);
 
-	return (
-		<>
-			<CardHeader className="text-2xl font-semibold text-secondary">
-				Photos
-			</CardHeader>
-			<Divider />
-			<CardBody>
-				<div className="grid grid-cols-5 gap-3">
-					{photos &&
-						photos.map((photo) => (
-							<div key={photo.id}>
-								<Image
-									width={300}
-									src={photo.url}
-									alt="Image of member"
-									className="object-cover aspect-square"
-								/>
-							</div>
-						))}
-				</div>
-			</CardBody>
-		</>
-	);
+	const renderedBody = () => {
+		return (
+			<div className="grid grid-cols-5 gap-3">
+				{photos &&
+					photos.map((photo) => (
+						<div key={photo.id}>
+							<Image
+								width={300}
+								src={photo.url}
+								alt="Image of member"
+								className="object-cover aspect-square"
+							/>
+						</div>
+					))}
+			</div>
+		);
+	};
+
+	return <CardInnerWrapper header="Photos" body={renderedBody()} />;
 };
 export default MemberPhotosPage;
