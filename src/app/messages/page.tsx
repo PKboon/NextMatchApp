1bde016094f61b48fe01d527ccce58eb4edfd7a3
Pikulkaew Boonpeng <51871643+PKboon@users.noreply.github.com>
2025-06-07 00:12:12 -0400

    add message's page sidebar

--- src/app/messages/page.tsx
+++ src/app/messages/page.tsx
@@ -1,5 +1,14 @@
+import MessageSidebar from "./MessageSidebar";
+
 const MessagesPage = () => {
-	return <div>MessagesPage</div>;
+	return (
+		<div className="grid grid-cols-12 gap-5 h-[80vh] mt-10">
+			<div className="col-span-2">
+				<MessageSidebar />
+			</div>
+			<div className="col-span-10">yay</div>
+		</div>
+	);
 };
 
 export default MessagesPage;
