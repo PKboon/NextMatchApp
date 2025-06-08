959c50f14c3fb7138e39e0cedc1f70ec7511c9f7
Pikulkaew Boonpeng <51871643+PKboon@users.noreply.github.com>
2025-06-08 12:57:41 -0400

    ui: filters

--- src/components/navbar/TopNav.tsx
+++ src/components/navbar/TopNav.tsx
@@ -6,6 +6,7 @@ import { GiMatchTip } from "react-icons/gi";
 import { getUserNavInfo } from "@/app/actions/userActions";
 import { auth } from "@/auth";
 
+import Filters from "./Filters";
 import NavLink from "./NavLink";
 import UserMenu from "./UserMenu";
 
@@ -20,55 +21,58 @@ const TopNav = async () => {
 	const userInfo = session?.user && (await getUserNavInfo());
 
 	return (
-		<Navbar
-			maxWidth="xl"
-			className="bg-gradient-to-r from-purple-400 to-purple-700"
-			classNames={{
-				item: [
-					"text-xl",
-					"text-white",
-					"uppercase",
-					"data-[active=true]:text-yellow-200",
-				],
-			}}
-		>
-			<NavbarBrand as={Link} href="/">
-				<GiMatchTip size={40} className="text-gray-200" />
-				<div className="font-bold text-3xl flex">
-					<span className="text-gray-900">Next</span>
-					<span className="text-gray-200">Match</span>
-				</div>
-			</NavbarBrand>
-			<NavbarContent justify="center">
-				{navLinks.map(({ href, label }) => (
-					<NavLink key={href} href={href} label={label} />
-				))}
-			</NavbarContent>
-			<NavbarContent justify="end">
-				{userInfo ? (
-					<UserMenu userInfo={userInfo} />
-				) : (
-					<>
-						<Button
-							as={Link}
-							href="/login"
-							variant="bordered"
-							className="text-white"
-						>
-							Login
-						</Button>
-						<Button
-							as={Link}
-							href="/register"
-							variant="bordered"
-							className="text-white"
-						>
-							Register
-						</Button>
-					</>
-				)}
-			</NavbarContent>
-		</Navbar>
+		<>
+			<Navbar
+				maxWidth="xl"
+				className="bg-gradient-to-r from-purple-400 to-purple-700"
+				classNames={{
+					item: [
+						"text-xl",
+						"text-white",
+						"uppercase",
+						"data-[active=true]:text-yellow-200",
+					],
+				}}
+			>
+				<NavbarBrand as={Link} href="/">
+					<GiMatchTip size={40} className="text-gray-200" />
+					<div className="font-bold text-3xl flex">
+						<span className="text-gray-900">Next</span>
+						<span className="text-gray-200">Match</span>
+					</div>
+				</NavbarBrand>
+				<NavbarContent justify="center">
+					{navLinks.map(({ href, label }) => (
+						<NavLink key={href} href={href} label={label} />
+					))}
+				</NavbarContent>
+				<NavbarContent justify="end">
+					{userInfo ? (
+						<UserMenu userInfo={userInfo} />
+					) : (
+						<>
+							<Button
+								as={Link}
+								href="/login"
+								variant="bordered"
+								className="text-white"
+							>
+								Login
+							</Button>
+							<Button
+								as={Link}
+								href="/register"
+								variant="bordered"
+								className="text-white"
+							>
+								Register
+							</Button>
+						</>
+					)}
+				</NavbarContent>
+			</Navbar>
+			<Filters />
+		</>
 	);
 };
 
